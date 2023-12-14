b5e406208f1eb53b9cd4d7e4cbb53cfae8da3d65
saad1551 <saad.ashraf.9094@gmail.com>
2023-12-14 21:03:01 +0500

    login and sign up functionality implemented

--- databaseproject/biddingsystem/urls.py
+++ databaseproject/biddingsystem/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create, name="create"),
-    path("listing/<str:listing_id>", views.listing, name="listing")
+    path("listing/<str:listing_id>", views.listing, name="listing"),
+    path("login", views.log_in, name="login"),
+    path("register", views.register, name="register"),
+    path("logout", views.log_out, name="logout")
 ]
--- databaseproject/biddingsystem/views.py
+++ databaseproject/biddingsystem/views.py
@@ -1,8 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection, IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+def my_custom_sql():
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT * FROM listing')
+        columns = [col[0] for col in cursor.description]
+        return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST["firstName"]
+        last_name = request.POST["lastName"]
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        try:
+            user = User.objects.create_user(first_name = first_name, last_name = last_name, email = email, username = username, password = password)
+            login(request, user)
+        except IntegrityError:
+            return render(request, "biddingsystem/register.html", {
+                    "message": "Username is already taken"
+                })         
+
+        return redirect(reverse(index))
+    
+    return render(request, "biddingsystem/register.html")
+
+def log_in(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        try:
+            user = authenticate(username=username, password=password)
+            login(request, user)
+        except:
+            return render(request, "biddingsystem/login.html", {
+                "message": "Invalid username or password"
+            })
+        
+        return redirect(reverse(index))
+    
+    return render(request, "biddingsystem/login.html")
+
+def log_out(request):
+    logout(request)
+
+    return redirect(reverse(index))
+
 def index(request):
-    return render(request, "biddingsystem/index.html")
+    listings = my_custom_sql()
+    return render(request, "biddingsystem/index.html", {
+        "listings": listings
+    })
 
 def create(request):
     return render(request, "biddingsystem/createListing.html")
